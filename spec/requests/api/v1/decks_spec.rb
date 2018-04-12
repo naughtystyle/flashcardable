0f904f95cb71b1b399917a698e45e502918b51a8
Dan <dansification@gmail.com>
2018-04-12 16:39:21 -0400

    Update deck spec
    Follow other specs format and ensure errors are returned

--- spec/requests/api/v1/decks_spec.rb
+++ spec/requests/api/v1/decks_spec.rb
@@ -40,12 +40,13 @@ RSpec.describe "GET /api/v1/decks/:id" do
   end
 
   context "when deck is not found and exception is raised" do
-    it "responds with a 404 not found status" do
+    it "responds with a 404 Not Found status" do
       nonexistent_id = 371
 
       get "/api/v1/decks/#{nonexistent_id}"
 
-      expect(response).to be_a_not_found
+      expect(response.status).to eq(404)
+      expect(json_body.fetch("errors")).not_to be_empty
     end
   end
 end
